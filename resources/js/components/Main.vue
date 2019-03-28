e6fa42cfad78f1faca32afb0bd9e6b4495637011
aminul <acterprime1@gmail.com>
2019-03-28 08:35:50 +0600

    after installation

---
+++ resources/js/components/Main.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>this is aminul</p>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
